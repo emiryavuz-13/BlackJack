e2dd0c13a371be536c08facfaf31d3100c89fb8c
Emir-yavuz <emirsemihzehra@gmail.com>
2024-12-01 18:56:13 +0300

    player sınıfı oluşturuldu

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from deck import Deck
+from player import Player
 def main():
     print("Welcome to BlackJack!")
+    deck = Deck()
+    player = Player("Oyuncu 1")
+    ai = Player("Yapay Zeka")
+
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+        self.score = 0
+
+    def add_card(self, card_name, card_value):
+        self.hand.append(card_name)
+        self.score += card_value
