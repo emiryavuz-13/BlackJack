408b32cb581f1496cca52d320822be8fcd6b96b3
Emir Yavuz <yvz.emir13@gmail.com>
2025-01-23 21:46:23 +0000

    Basit düzeltmeler

--- player.py
+++ player.py
@@ -3,7 +3,6 @@ class Player:
         self.name = name
         self.hand = []
         self.score = 0
-
     def add_card(self, card_name, card_value):
         self.hand.append(card_name)
         self.score += card_value
